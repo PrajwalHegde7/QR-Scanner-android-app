a03067ce7fdbaf087ed13ed27e5dda0824bb21c2
Prajwal Hegde <prajwalsh234@gmail.com>
2024-03-19 19:57:40 +0530

    Updated

--- app/src/androidTest/java/com/pstech/qrscanner/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/pstech/qrscanner/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.qrscanner", appContext.getPackageName());
+        assertEquals("com.pstech.qrscanner", appContext.getPackageName());
     }
 }
